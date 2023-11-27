323e5296496a0a972c629e95cb5dc657b23c8d6a
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2023-11-27 23:21:10 +0530

    Setup mock backend

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:3000/tasks';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
