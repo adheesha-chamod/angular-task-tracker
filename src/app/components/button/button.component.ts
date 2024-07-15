a561b4526d47f5baf879da11b5ad984f3dafe3b1
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-07-15 19:49:10 +0530

    Add header

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,14 @@
+import {
+  Component,
+  Input
+} from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  @Input() text: string | undefined;
+  @Input() color: string | undefined;
+}
